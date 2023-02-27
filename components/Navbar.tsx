30a355b25d1b2ba854e72fcfc70a238ffc58199d
Dennis Codes <dennis@ngeni.io>
2023-02-28 02:43:20 +0300

    added router to navigate to links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,10 @@ import {
 } from "./Navbar.Styles";
 import Logo from "../public/Logo.svg";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter()
   return (
     <NavbarWrapper className="navbar-theme">
       <NavbarGrid>
@@ -17,7 +19,7 @@ export default function Navbar() {
         </NavbarHeader>
         <Container style={{padding: "0"}}>
           <NavbarText>Developer Services</NavbarText>
-          <NavbarText>Acadamey</NavbarText>
+          <NavbarText onClick={() => router.push('/academy')}>Acadamey</NavbarText>
           <NavbarText>Design Services</NavbarText>
           <NavbarText>Case Studies</NavbarText>
         </Container>
